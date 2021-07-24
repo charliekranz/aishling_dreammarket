a6f92c53c1ce14d2abdeb61a577b889cb483a587
charliekranz <charliekranz3@gmail.com>
2021-07-24 11:15:06 +0000

    Finishing Deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn aishling_dreamshop.wsgi:application
--- aishling_dreammarket/settings.py
+++ aishling_dreammarket/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-im&7996qi1u16blmv^6b_8$z$iel5z2$urozhpn2kh-ra%s$as
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ckranz-aishling-dreamshop.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -117,14 +117,20 @@ WSGI_APPLICATION = 'aishling_dreammarket.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+
 
 
 # Password validation
