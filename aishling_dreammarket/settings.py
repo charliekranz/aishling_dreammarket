c91b521a25242f9b0377fc86d90d8acf56e1e62e
charliekranz <charliekranz3@gmail.com>
2021-07-24 11:54:35 +0000

    Removed Secret Key and set Debug

--- aishling_dreammarket/settings.py
+++ aishling_dreammarket/settings.py
@@ -18,13 +18,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+# See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-im&7996qi1u16blmv^6b_8$z$iel5z2$urozhpn2kh-ra%s$as'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['ckranz-aishling-dreamshop.herokuapp.com', 'localhost']
 
