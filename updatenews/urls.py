98f05a90f99f8249e4e221a5bd9de5d105426912
charliekranz <charliekranz3@gmail.com>
2021-07-29 08:07:22 +0000

    Adjusted urls in updatenews

--- updatenews/urls.py
+++ updatenews/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('updatenews.html', views.updatenews, name='updatenews'),
-    path('/updatenews/updatenews.html', views.PostList.as_view(), name='updatenews'),
+    path('', views.PostList.as_view(), name='updatenews'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
 ]
